ecae8528aab3744a7b8365920a33d0d8c4aad241
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-09-09 20:51:51 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <title>Projeto/Mural</title>
+    <title>Projeto</title>
     <!--bot css-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!--bot css-->
@@ -55,11 +55,18 @@
     }  
     h3{
         color: white;
+        
     }
     div{
         background-color: black;
+        text-align: center;
+        color: #069;
     }
-    
+    footer{
+      background-color: #069;
+    }
+  
+
 </style>
 </head>
 <body>
@@ -132,56 +139,72 @@
 
 <!--Trailer do Jogo-->
 <center><br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/gc-VT5hOciI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</center>    
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qIQ3xNqkVC4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</center><br>
 <!--Trailer do Jogo-->
 
-<!--Time da empresa-->
+<!--Tabela de jogos-->
+
+<nav class="navbar navbar-light bg-black"><br>
+  <div class="container">
+    <form class="d-flex">
+      <input class="form-control me-2" type="Pesquisar por jogo" placeholder="Pesquisar por jogo" aria-label="Pesquisar por jogo">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
 <div id="team-area">
         <div class="container">
           <div class="row">
             <div class="col-12">
-              <h3 class="main-title">TABELA DE JOGOS</h3>
+              <h3 class="main-title"></h3><br>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3"><br>
               <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/pt/1/10/Battlefield_5_capa.png" class="card-img-top" alt="Imagem de Perfil 1">
+                <img src="https://cdn1.epicgames.com/b30b6d1b4dfd4dcc93b5490be5e094e5/offer/RDR2476298253_Epic_Games_Wishlist_RDR2_2560x1440_V01-2560x1440-2a9ebe1f7ee202102555be202d5632ec.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 1">
                 <div class="card-body">
                   <h5 class="card-title"></h5>
-                  <p class="card-text">JOGO BATTLEFIELD V VALOR: R$150,00
+                  <p class="card-text">RED DEAD REDEMPTION
+                  <p class="card-text">VALOR R$200,00 
+                                </p>
+                                </P>
+                                <a href="#" class="btn btn-primary">COMPRAR</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3"><br>
+              <div class="card">
+                <img src="https://criticalhits.com.br/wp-content/uploads/2021/05/GTA-5-Online.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 2">
+                <div class="card-body">
+                  <h5 class="card-title"></h5>
+                  <p class="card-text"> GRAND THEFT AUTO V
+                  <p class="card-text"> VALOR: R$100,00
+                                </p>
                                 </p>
                                 <a href="#" class="btn btn-primary">COMPRAR</a>
                 </div>
               </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3"><br>
               <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/pt/1/10/Battlefield_5_capa.png" class="card-img-top" alt="Imagem de Perfil 2">
+                <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1506830/header.jpg?t=1630705401" height="150" width="100"  class="card-img-top" alt="Imagem de Perfil 3">
                 <div class="card-body">
                   <h5 class="card-title"></h5>
-                  <p class="card-text">JOGO BATTLEFIELD V VALOR: R$150,00
+                  <p class="card-text">FIFA 2022 
+                  <p class="card-text">VALOR: R$200,00
                                 </p>
                                 <a href="#" class="btn btn-primary">COMPRAR</a>
                 </div>
               </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3"><br>
               <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/pt/1/10/Battlefield_5_capa.png" class="card-img-top" alt="Imagem de Perfil 3">
+                <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/0715/BvFOUf5GL9asjD0VNNNClG4w.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 4">
                 <div class="card-body">
                   <h5 class="card-title"></h5>
-                  <p class="card-text">JOGO BATTLEFIELD V VALOR: R$150,00
-                                </p>
-                                <a href="#" class="btn btn-primary">COMPRAR</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="card">
-                <img src="https://upload.wikimedia.org/wikipedia/pt/1/10/Battlefield_5_capa.png" class="card-img-top" alt="Imagem de Perfil 4">
-                <div class="card-body">
-                  <h5 class="card-title"></h5>
-                  <p class="card-text">JOGO BATTLEFIELD V VALOR: R$150,00
+                  <p class="card-text">NEED FOR SPEED HEAT 
+                  <p class="card-text"> VALOR: R$150,00
                                 </p>
                                 <a href="#" class="btn btn-primary">COMPRAR</a>
                 </div>
@@ -191,7 +214,132 @@
         </div>
       </div>
 
-<!--Time da empresa-->
+
+      <div id="team-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <h3 class="main-title"></h3><br>
+            </div>
+            <div class="col-md-3"><br>
+              <div class="card">
+                <img src="https://cdn2.unrealengine.com/egs-battlefield2042goldedition-dice-editions-g1a-00-1920x1080-8c5f388aac93.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 1">
+                <div class="card-body">
+                  <h5 class="card-title"></h5>
+                  <p class="card-text">BATTLEFIELD 2042
+                  <p class="card-text">VALOR R$200,00 
+                                </p>
+                                </P>
+                                <a href="#" class="btn btn-primary">COMPRAR</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3"><br>
+              <div class="card">
+                <img src="https://s2.glbimg.com/33wqBaejqW1pfaQFTQZ9S2t8wko=/0x0:783x391/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/v/t/IPHBU6SGiSO9BOA5VVwA/mortal-kombat-11.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 2">
+                <div class="card-body">
+                  <h5 class="card-title"></h5>
+                  <p class="card-text"> MORTAL KOMBAT XI
+                  <p class="card-text"> VALOR: R$150,00
+                                </p>
+                                </p>
+                                <a href="#" class="btn btn-primary">COMPRAR</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3"><br>
+              <div class="card">
+                <img src="https://olhardigital.com.br/wp-content/uploads/2021/06/PES-2022.jpg" height="150" width="100"  class="card-img-top" alt="Imagem de Perfil 3">
+                <div class="card-body">
+                  <h5 class="card-title"></h5>
+                  <p class="card-text">PES 2022 
+                  <p class="card-text">VALOR: R$200,00
+                                </p>
+                                <a href="#" class="btn btn-primary">COMPRAR</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3"><br>
+              <div class="card">
+                <img src="https://s2.glbimg.com/O5rgTt_kxYNxHM_uIkaaQWlovXU=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/0/a/tWNcCTTAKfrgNBUXgVag/star-wars-jedi-fallen-order-gameplay-techtudo.jpg" height="150" width="100" class="card-img-top" alt="Imagem de Perfil 4">
+                <div class="card-body">
+                  <h5 class="card-title"></h5>
+                  <p class="card-text">STAR WARS JEDI: FALLEN ORDER
+                  <p class="card-text"> VALOR: R$150,00
+                                </p>
+                                <a href="#" class="btn btn-primary">COMPRAR</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<!--Tabela de jogos-->
+
+
+
+<div id="apply-area"><br>
+        <div class="container-fluid"><br>
+          <div class="row">
+            <center>
+            <div class="col-md-4 apply-box" id="company-img"></div>
+            <div class="col-md-2 apply-box" id="pattern-img">
+            </center> 
+              <h4>Gosta de desafios?</h4>
+              <p>Temos várias posições abertas, entre elas: Software Engineer, UI/UX Designer, Project Manager e mais.</p>
+              <p>Clique no botão abaixo e confira os benefícios e as posições em aberto.</p>
+              <p>Aqui você terá a chance de trabalhar com as melhores tecnologias e os profissionais mais capacitados.</p>
+              <a href="#" class="main-btn" id="apply-btn">Saiba Mais</a><br>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    <footer><br>
+    <div id="contact-area"><br> 
+      <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h3 class="main-title">Entre em contato conosco</h3>
+            </div>
+            <div class="col-md-4 contact-box">
+              <i class="fas fa-phone"></i>
+              <p><span class="contact-tile">Ligue para:</span> (47)9 9610-5977</p>
+              <p><span class="contact-tile">Horários:</span> 8:00 - 19:00</p>
+            </div>
+            <div class="col-md-4 contact-box">
+              <i class="fas fa-envelope"></i>
+              <p><span class="contact-tile">Envie um email:</span> faelsouza.rsg@hotmail.com</p>
+            </div>
+            <div class="col-md-4 contact-box">
+              <i class="fas fa-map-marker-alt"></i>
+              <p><span class="contact-tile">Venha tomar um café:</span> Rua Selma Doering Bruns - 361</p>
+            </div>
+            <div class="col-md-6" id="msg-box">
+              <p>Ou nos deixe uma mensagem:</p>
+            </div>
+            <div class="col-md-6" id="contact-form">
+              <form action="">
+                <input type="text" class="form-control" placeholder="E-mail" name="email">
+                <input type="text" class="form-control" placeholder="Assunto" name="subject">
+                <textarea class="form-control" rows="3" placeholder="Sua mensagem..." name="message"></textarea>
+                <input type="submit" class="main-btn">
+              </form>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div id="copy-area">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+              <p>Desenvolvido por <a href="https://www.horadecodar.com.br" target="_blank">Lucas Rafael</a> &copy; 2021</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 
 </body>
 </html>
